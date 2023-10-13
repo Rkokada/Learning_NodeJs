1b348eeb8ba4a07c3fe8d0f20ffabb3ee4273faa
RamondRocha <110187632+Rkokada@users.noreply.github.com>
2023-10-13 13:45:46 -0300

    First commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+import { memoriesRoutes } from "./routes/memories";
+
+
+const app = fastify();
+
+
+app.register(memoriesRoutes)
+
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP server running on http://localhost:3333");
+  });
